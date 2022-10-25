1088c7aadd77a78b28c152684e1e959293ed6e30
dxArtemiusz <artem.kazakov@devexpress.com>
2022-10-25 16:48:49 +0400

    Added Vue Project Template

---
+++ vue/src/components/theme-selector.vue
@@ -0,0 +1,30 @@
+<template>
+    <DxSelectBox 
+        :items="themeSource"
+        :value="currentTheme"
+        valueExpr="value"
+        displayExpr="text"
+        @value-changed="onValueChanged"/>
+</template>
+<script>
+import theme from '../services/theme-service';
+import DxSelectBox from 'devextreme-vue/select-box';
+export default {
+  components: {
+    DxSelectBox,
+  },
+  data() {
+    let currentTheme = theme.getTheme()
+    let themeSource = theme.getThemeData()
+    return {
+        currentTheme,
+        themeSource
+    };
+  },
+  methods:{
+    onValueChanged(e){
+      theme.applyTheme(e.value)
+    }
+  }
+};
+</script>
---
+++ vue/src/main.js
@@ -0,0 +1,15 @@
+import './themes/generated/theme.additional.css';
+
+import { createApp }  from "vue";
+import router from "./router";
+
+
+import App from "./App";
+import appInfo from "./app-info";
+
+
+
+const app = createApp(App);
+app.use(router);
+app.config.globalProperties.$appInfo = appInfo;
+app.mount('#app');
